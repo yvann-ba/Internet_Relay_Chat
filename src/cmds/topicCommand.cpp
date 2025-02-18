d2326761168ea5227d126b3dc4dccaeaa6c49245
yvann-ba <barbot.yvann@gmail.com>
2025-02-18 11:24:17 +0100

    improve topic

--- src/cmds/topicCommand.cpp
+++ src/cmds/topicCommand.cpp
@@ -1,5 +1,8 @@
 #include "../../includes/server.hpp"
 
+// Process the TOPIC command: view or change the channel topic.
+// If a new topic is provided and the channel has topic restrictions enabled,
+// only channel operators are allowed to modify the topic.
 void Server::topicCommand(const std::string &parameters, int client_fd) {
     size_t spacePos = parameters.find(" ");
     std::string channelName, topic;
@@ -28,10 +31,17 @@ void Server::topicCommand(const std::string &parameters, int client_fd) {
         return;
     }
     
+    // If no new topic is provided, send the current topic to the client.
     if (topic.empty()) {
         std::string reply = ":" + _clients[client_fd]->getNickname() + " TOPIC " + channelName + " :" + channel->getTopic() + "\r\n";
         sendServ(_clients[client_fd]->getFDSocket(), reply);
     } else {
+        // If the channel is topic restricted, only operators can change the topic.
+        if (channel->isTopicRestricted() && !channel->isOperator(client_fd)) {
+            sendError(client_fd, 482, channelName + " :You're not a channel operator");
+            return;
+        }
+        // Update the topic and broadcast the change.
         channel->setTopic(topic);
         std::string topicMsg = ":" + _clients[client_fd]->getNickname() + " TOPIC " + channelName + " :" + topic + "\r\n";
         channel->broadcastMessage(topicMsg, client_fd);
